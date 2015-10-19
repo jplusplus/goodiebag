eaeb2aead5b6d3661cce8b3d2209f7863ce1dce5
rotsee <leo_wallentin@hotmail.com>
2015-10-19 07:23:18 +0200

    rename

--- lists.py
+++ lists.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 
 
+def get_unique(list_):
+    """Returnerar en lista där varje värde bara förekommer
+     en gång.
+    """
+    return list(set(list_))
+
+
 def flatten_list(list_):
     """ Returnerar en endimensionell lista [a, b, c, d, e],
      givet en tvådimensionell [[a, b], [c], [d, e]]
@@ -10,3 +17,5 @@ def flatten_list(list_):
             for inner in outer]
 
 print(flatten_list([[1, 2], [3], [4, 5]]))
+
+print([1, 2, 3, 1, 6, 1, 4, 5])
